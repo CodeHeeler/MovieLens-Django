f077440a04f2c278e70a74a4e7c9f90c95b24203
RebelMerf <rebelmurf@gmail.com>
2017-03-17 16:10:43 -0400

    worked on additional views and urls to match the new site structure

--- movie_lens/recommender/urls.py
+++ movie_lens/recommender/urls.py
@@ -5,10 +5,24 @@ from . import views
 urlpatterns = [
     # GET /recommender/
     url(r'^$', views.index, name='index'),
-    # GET /recommender/5/
-    url(r'^(?P<movie_id>[0-9]+)/$', views.detail, name='detail'),
-    # GET /recommender/5/rating/
-    url(r'^(?P<movie_id>[0-9]+)/ratings/$', views.ratings, name='ratings'),
-    # GET /recommender/5/rate/
-    url(r'^(?P<movie_id>[0-9]+)/rate/$', views.rate, name='rate'),
+    # # GET /recommender/login/
+    # url(r'^login/$', views.login, name='login'),
+    # # GET /recommender/login_success/
+    # url(r'^login_success/$', views.login_success, name='login_success'),
+
+    # GET /recommender/movies/
+    url(r'^movies/$', views.movies, name='movies'),
+    # GET /recommender/movies/5/
+    url(r'^movies/(?P<movie_id>[0-9]+)/$', views.movie_detail, name='movie_detail'),
+    # GET /recommender/movie/5/
+    url(r'^users/$', views.users, name='users'),
+    # GET /recommender/users/5/
+    url(r'^users/(?P<user_id>[0-9]+)$', views.user_detail, name='user_detail'),
+    # GET /recommender/movies/5/rating/
+    url(r'^movies/(?P<movie_id>[0-9]+)/ratings/$', views.ratings, name='ratings'),
+    # GET /recommender/movies/5/rate/
+    url(r'^(?P<movie_id>[0-9]+)/rate/$', views.rate, name='rate')
+
+    # # GET /recommender/login_error/
+    # url(r'^login_error/$', views.login_error, name='login_error')
 ]
--- movie_lens/recommender/views.py
+++ movie_lens/recommender/views.py
@@ -1,25 +1,77 @@
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
 # from django.template import loader
 from django.db.models import Count, Avg
 from .models import Movie
 
 
 def index(request):
+    return render(request, 'recommender/index.html')
+
+
+# def login(request):
+#     username = request.POST['username']
+#     password = request.POST['password']
+#     user = authenticate(username=username, password=password)
+#     if user is not None:
+#         login(request, user)
+#         # Redirect to a success page.
+#         return render(request, 'recommender/login_success.html')
+#     else:
+#         # Return an 'invalid login' error message.
+#         return render(request, 'recommender/login_error.html')
+#     return render(request, 'recommender/login.html')
+
+#
+# def login_error(request):
+#     #login error stuff
+#     return HttpResponse("There was an error with your login.")
+#
+#
+# def login_success(request):
+#     #login success stuff
+#     return HttpResponse("Success! You are now logged in.")
+
+
+def top_20(request):
+    # if not request.user.is_authenticated:
+    #     return redirect('%s?next=%s' % (settings.LOGIN_URL, request.path))
     top = Movie.objects.annotate(avg=Avg('rating'), rate_count=Count('rating')).order_by('-rate_count')[:20]
-    return render(request, 'recommender/index.html', {'top20': top})
+    return render(request, 'recommender/top_20.html', {'top20': top})
 
 
-def detail(request, movie_id):
+def movies(request):
+    return render(request, 'recommender/movies.html')
+
+
+def movie_detail(request, movie_id):
+    # if not request.user.is_authenticated:
+    #     return redirect('%s?next=%s' % (settings.LOGIN_URL, request.path))
     movie = get_object_or_404(Movie, pk=movie_id)
-    return render(request, 'recommender/detail.html', {'movie': movie, 'num_rating': movie.num_of_ratings(), 'avg': movie.average_rating()})
+    return render(request, 'recommender/movie_detail.html', {'movie': movie, 'num_rating': movie.num_of_ratings(), 'avg': movie.average_rating()})
+
+
+def users(request):
+    return render(request, 'recommender/users.html')
+
+
+def user_detail(request, user_id):
+    # if not request.user.is_authenticated:
+    #     return redirect('%s?next=%s' % (settings.LOGIN_URL, request.path))
+    user = get_object_or_404(User, pk=user_id)
+    return render(request, 'recommender/user_detail.html', {'user': user})
 
 
 def ratings(request, movie_id):
+    # if not request.user.is_authenticated:
+    #     return redirect('%s?next=%s' % (settings.LOGIN_URL, request.path))
     response = "You're looking at the results of movie %s."
     return HttpResponse(response % movie_id)
 
 
 def rate(request, movie_id):
+    # if not request.user.is_authenticated:
+    #     return redirect('%s?next=%s' % (settings.LOGIN_URL, request.path))
     return HttpResponse("You're voting on movie %s." % movie_id)
